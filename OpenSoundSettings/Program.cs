13aa39d7d7de2848a84b9c0cab6aa22b59b73c12
Sahid Fakiry <s.fakiry23@gmail.com>
2024-08-28 21:45:37 +0200

    Add project files.

---
+++ OpenSoundSettings/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+
+namespace OpenSoundSettings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ExecuteFile(@"control.exe mmsys.cpl,,1");
+        }
+
+        public static void ExecuteFile(string command)
+        {
+            ProcessStartInfo processInfo = new ProcessStartInfo(@"cmd.exe", "/c" + command);
+            Process process;
+
+            processInfo.CreateNoWindow = true;
+            processInfo.UseShellExecute = false;
+
+            process = Process.Start(processInfo);
+
+            process.Close();
+        }
+    }
+}
